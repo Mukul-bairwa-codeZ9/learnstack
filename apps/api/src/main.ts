3dc563e62f92008c9e5377e314f9350bf3885c7e
Mukul-bairwa-codeZ9 <mukul.bairwa.dev98@gmail.com>
2026-05-27 22:43:33 +0530

    chore: initial app setup and convert to monorepo architecture

---
+++ apps/api/src/main.ts
@@ -0,0 +1,46 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import compression from 'compression';
+import helmet from 'helmet';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  app.use(helmet());
+
+  // app.use(compression());
+
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Developer Docs Platform API')
+    .setDescription('Production API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(4000);
+
+  console.log(`API running on http://localhost:4000`);
+}
+
+bootstrap();
