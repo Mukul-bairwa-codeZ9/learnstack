48d7784bc2428ea17c830964d8d144a9d0446310
Mukul-bairwa-codeZ9 <mukul.bairwa.dev98@gmail.com>
2026-06-24 16:06:17 +0530

    docs(api): harden swagger documentation

--- apps/api/src/modules/health/health.controller.ts
+++ apps/api/src/modules/health/health.controller.ts
@@ -1,17 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
 
 import { HealthService } from './health.service';
-
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+@ApiTags('Healh Check')
 @Controller('health')
 export class HealthController {
   constructor(private readonly healthService: HealthService) {}
 
   @Get()
+  @ApiOperation({
+    summary: 'Health status',
+  })
   getHealth() {
     return this.healthService.getHealth();
   }
 
   @Get('ready')
+  @ApiOperation({
+    summary: 'Server ready status',
+  })
   getReadiness() {
     return this.healthService.getReadiness();
   }
--- apps/api/src/modules/public-content/public-content.controller.ts
+++ apps/api/src/modules/public-content/public-content.controller.ts
@@ -3,12 +3,17 @@ import { Controller, Get, Param, Query } from '@nestjs/common';
 import { PublicContentService } from './public-content.service';
 import { PublicDocumentResponseDto } from './dto/public-document-response.dto';
 import { PublicDocumentsQueryDto, PublicDocumentsResponseDto } from './dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Public Content')
 @Controller('public-content')
 export class PublicContentController {
   constructor(private readonly publicContentService: PublicContentService) {}
 
   @Get()
+  @ApiOperation({
+    summary: 'Get all public content based on filters',
+  })
   async findAll(
     @Query() query: PublicDocumentsQueryDto,
   ): Promise<PublicDocumentsResponseDto> {
@@ -16,6 +21,9 @@ export class PublicContentController {
   }
 
   @Get(':slug')
+  @ApiOperation({
+    summary: 'Get public content by slug value',
+  })
   async findBySlug(
     @Param('slug') slug: string,
   ): Promise<PublicDocumentResponseDto> {
